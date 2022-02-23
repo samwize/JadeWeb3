fc129d5f316047556cd95705eb94420ebe0ef45e
Junda <junda@just2us.com>
2022-02-23 16:57:21 +0800

    tailwind testpage

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import PrimaryButton from './components/PrimaryButton.vue';
+import TestPage from './components/TestPage.vue';
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello tailwind!
-  </h1>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <TestPage />
 </template>
 
 <style>
---
+++ src/components/TestPage.vue
@@ -0,0 +1,54 @@
+<script setup>
+import PrimaryButton from './PrimaryButton.vue';
+</script>
+
+<template>    
+  <h1 class="text-3xl font-bold underline pt-10">
+    Hello tailwind!
+  </h1>
+
+  <div class="flex-1 items-center">
+    <img class="h-12" src="/assets/logo.png">
+    <div>
+      <strong>Junda Ong</strong>
+      <span>Full Slack</span>
+    </div>
+  </div>
+
+  <PrimaryButton>I am a button</PrimaryButton>
+
+  <blockquote class="text-2xl font-semibold italic text-center text-slate-900">
+    When you look
+    <span class="relative">
+      <span class="block absolute -inset-1 -skew-y-3 bg-pink-500" aria-hidden="true"></span>
+      <span class="relative text-white">annoyed</span>
+    </span>
+    all the time, people think that you're busy.
+  </blockquote>
+
+  <label class="relative block p-5">
+    <span class="sr-only">Search</span>
+    <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+      <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"><!-- ... --></svg>
+    </span>
+    <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
+  </label>
+  
+  <label class="block">
+    <span class="sr-only">Select photo</span>
+    <input type="file" class="block w-full text-sm text-slate-500
+      file:mr-4 file:py-2 file:px-4
+      file:rounded-full file:border-0
+      file:text-sm file:font-semibold
+      file:bg-violet-50 file:text-violet-700
+      hover:file:bg-violet-100
+    "/>
+  </label>
+
+  <!-- Full-width fluid until the `md` breakpoint, then lock to container -->
+  <div class="md:container md:mx-auto">
+    <div class="text-left bg-slate-500">
+      <p>Hello</p>
+    </div>
+  </div>
+</template>
